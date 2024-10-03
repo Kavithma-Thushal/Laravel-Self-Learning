a961042161bb6e941e459bb220661ee150959422
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-03 15:48:51 +0530

    Email Names Refactored 2

--- resources/views/emails/email_form.blade.php
+++ resources/views/emails/email_form.blade.php
@@ -38,6 +38,5 @@
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 </body>
 </html>
--- resources/views/emails/email_template.blade.php
+++ resources/views/emails/email_template.blade.php
@@ -1,13 +1,1 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel-Self-Learning</title>
-</head>
-
-<body>
 <p>{{ $details['message'] }}</p>
-
-</body>
-</html>
