49e33835885eaffd84c79ecbaeffb8c66a8708b2
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-10-24 14:29:03 +0530

    route updated

--- Authentication/routes/web.php
+++ Authentication/routes/web.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 
-Route::get('/', function () {return view('welcome');});
+Route::middleware(['auth'])->get('/welcome', function () {
+    return view('welcome');
+});
