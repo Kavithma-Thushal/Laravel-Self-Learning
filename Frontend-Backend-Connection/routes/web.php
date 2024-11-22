bc85ade0d00bc24d3ed63c8723163c72ec0fd4cb
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-11-22 21:17:52 +0530

    Customer View and Customer Table Added

--- Frontend-Backend-Connection/routes/web.php
+++ Frontend-Backend-Connection/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'customer'], function () {
+    Route::get('/view', [CustomerController::class, 'view']);
 });
